050201621c12d8eef7b3d0e5f1c511b7bc532ccf
Hamza Jassar <jassarhamza@gmail.com>
2022-07-11 13:50:59 -0500

    implemented modules

---
+++ scripts/config.js
@@ -0,0 +1,2 @@
+export const APP_ID = '6d15fedd68a149451a4f1650a932fa18';
+export const API_URL = `https://api.openweathermap.org/data/2.5/weather?appid=${APP_ID}`;
---
+++ scripts/elements.js
@@ -0,0 +1,10 @@
+export const location_form = document.getElementById('location-form');
+export const searchField_input = document.getElementById('search-field');
+export const getLocation_btn = document.getElementById('get-location');
+
+export const tempValue_el = document.getElementById('temp-value');
+export const location_el = document.getElementById('location');
+export const tempIcon_el = document.getElementById('temp-icon');
+
+export const error_el = document.getElementById('error');
+export const loading_el = document.getElementById('loading');
